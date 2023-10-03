b8a14608b1643817afb292fa5aa6492d1f94c51b
senior9 <ovi215188@gmail.com>
2023-10-04 02:21:54 +0600

    navbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import DarkModeProvider from "./DarkModeProvider";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <DarkModeProvider>
           <Header />
-
+          <Navbar/>
           {children}
         </DarkModeProvider>
       </body>
