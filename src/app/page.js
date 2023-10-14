05b96048d14a21843aee4228b3f615ffeff53474
senior9 <ovi215188@gmail.com>
2023-10-15 05:53:35 +0600

    Movie card design complete

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,13 @@ import ResultsData from "@/components/ResultsData";
 export default async function Home({searchParams}) {
   const API_KEY = process.env.API_KEY;
   console.log(API_KEY)
-  const genre = searchParams.genre || "fetchTrending";
+  const genre = searchParams.genre || "fetchTopRated";
   const res = await fetch(`https://api.themoviedb.org/3/${
-    genre === "fetchTrending" ? "movie/top_rated" : "trending/all/week"}?&api_key=${API_KEY}&language=en-US&page=1`,{ next: { revalidate: 10000 } });
+    genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?&api_key=${API_KEY}&language=en-US&page=1`,{ next: { revalidate: 10000 } });
   // console.log(res)
   const data = await res.json();
   const results = data.results;
-  // console.log(results);
+  console.log(results);
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,41 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { FiThumbsUp } from 'react-icons/fi';
 
 const Card = ({result}) => {
     return (
-        <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
-            <p>{result.id}</p>
+        <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
+            <Link 
+            href={`/movie/${result.id}`}
+            >
+                <Image
+                src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
+                width={500}
+                height={500}
+                style={
+                    {maxWidth:'100%',
+                    height:'auto',
+                }}
+                className='sm:rounded-t-lg group-hover:opacity-40 transition-opacity duration-300'
+                placeholder='blur'
+                blurDataURL='/public/spinner.svg'
+                >
+                    
+                </Image>
+            </Link>
+
+            <div className='p-2'>
+                 <p className='line-clamp-2'>
+                    {result.overview}
+                 </p>
+                 <p className='truncate font-bold text-lg'>{result.title || result.name } </p>
+                 <p className='flex items-center'>
+                 {result.release_date || result.first_air_date}
+                 <FiThumbsUp className='h-5 mr-1 ml-3'/>
+                 {result.vote_count}
+                 </p>
+            </div>
         </div>
     );
 };
