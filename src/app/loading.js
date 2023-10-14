421198d201df9821f0a2e49c1f574571ba6240ac
senior9 <ovi215188@gmail.com>
2023-10-15 04:31:27 +0600

    loading use in next js

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='flex justify-center'>
+           <img className='' src='spinner.svg' alt='loading...'></img>
+        </div>
+    );
+};
+
+export default Loading;
