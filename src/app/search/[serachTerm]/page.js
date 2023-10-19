66d1e9bc7fa7f0919a43c8f316759deacdcf7ed2
senior9 <ovi215188@gmail.com>
2023-10-20 04:46:46 +0600

    Unfortunately Search keword may not be work its under development

--- src/app/search/[serachTerm]/page.js
+++ src/app/search/[serachTerm]/page.js
@@ -2,13 +2,13 @@ import ResultsData from "@/components/ResultsData";
 
 export default async function SearchPage({ params }) {
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&include_adult=false`
-  );
+    `https://api.themoviedb.org/3/search/movie?query=${params.searchTerm}&api_key=${process.env.API_KEY}&language=en-US`);
   if (!res.ok) {
     throw new Error("Error to get data");
   }
   const data = await res.json();
   const results = data.results;
+  console.log(results);
 
   return (
     <div>
